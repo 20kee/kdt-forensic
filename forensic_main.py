b2659eafc40ce50ff83bc177534ecaa6a9775af2
20kee <20kee@naver.com>
2023-11-25 17:32:22 +0900

    전체적인 틀 완성, ntfs 포렌식 리팩토링 완성

--- forensic_main.py
+++ forensic_main.py
@@ -17,22 +17,34 @@ def main():
         global window_opt
         window_opt = selection
 
-    opt_dropdown = OptionMenu(window, variable, "window xp", "window 7", "window 8", "window 10", "window 11", command=opt_update)
+    opt_dropdown = OptionMenu(window, variable, *window_names, command=opt_update)
     opt_dropdown.place(x=10, y=10)
 
     def forensic_main():
-        forensic_tool = ForensicTool(window_names[window_opt])
+        forensic_tool = ForensicTool(window_names[window_opt], drive=drive_entry.get())
         forensic_tool.SetLink()
         if recycle_bin_var.get():
             forensic_tool.RecycleBinForensic()
+        if ntfs_var.get():
+            forensic_tool.NTFSForensic()
+
+    drive_label = Label(window, text='드라이브')
+    drive_label.place(x=120, y=13)
+    drive_entry = Entry(window, width=2)
+    drive_entry.insert(0, 'C')
+    drive_entry.place(x=180, y=13)
+
 
     start_button = Button(window, text="포렌식 시작", command= forensic_main)
-    start_button.place(x=100, y=10)
+    start_button.place(x=10, y=80)
 
     recycle_bin_var = IntVar()
     recycle_bin_check = Checkbutton(window, text='휴지통 포렌식', variable=recycle_bin_var)
-    recycle_bin_check.place(x=10, y=40)
-    
+    recycle_bin_check.place(x=10, y=45)
+
+    ntfs_var = IntVar()
+    ntfs_check = Checkbutton(window, text='NTFS', variable=ntfs_var)
+    ntfs_check.place(x=130, y=45)
     
     window.mainloop()
 
--- forensictool/recyclebin.py
+++ forensictool/recyclebin.py
@@ -1,25 +1,22 @@
 import os
 from datetime import datetime
+from forensictool.ntfs_forensic import Extract_File
+import hashlib
 
 class ForensicTool:
-    def __init__(self, window_ver):
-        self._recycle_bin_link = 'C:\\$Recycle.Bin'
+    def __init__(self, window_ver, drive):
+        self._drive = drive
+        self._recycle_bin_link = drive + ':\\$Recycle.Bin'
         self._window_ver = window_ver
         self._recycle_bin_link = None
+        self._NTFS_tool = Extract_File(drive + ':')
 
     def SetLink(self):
         if self._window_ver == "window 11":
             print(1)
             self._recycle_bin_link = 'C:\\$Recycle.Bin'
     
-    def Bytes2Int(self, bytes):
-        result = 0
-        for byte in bytes:
-            result *= 256
-            result += int(byte)
-        return result
-
-    def RecycleBinForensic(self):
+    def RecycleBinForensic(self): # 휴지통 포렌식
         result_file = open('휴지통 포렌식 결과.txt', 'w')
         if self._window_ver == "window 11":
             dirs = os.listdir(self._recycle_bin_link) # 휴지통 안의 폴더들
@@ -33,7 +30,7 @@ class ForensicTool:
                             data = f.read()
                             print(int.from_bytes(data[16:24], 'little') / 1000)
                             result_file.write('파일명 : ' + file + '\n파일 헤더 : ' + str(int.from_bytes(data[:8], 'little')) + '\n원본 파일 크기 : ' + str(int.from_bytes(data[8:16], 'little')) + '\n삭제된 시간 : ' + str(datetime.fromtimestamp(int.from_bytes(data[16:24], 'little') / 1e9)) + '\n\n')
-                            
+                            # 삭제된 시간 수정 필요함
                             f.close()
 
                 except Exception as e:
@@ -42,3 +39,14 @@ class ForensicTool:
         
         print('Recycle Bin Forensic Done.')
         result_file.close()
+
+    def NTFSForensic(self): # NTFS 포렌식
+        self._NTFS_tool.Extract('/$MFT','$MFT')
+        self._NTFS_tool.Extract('/$LogFile','$LogFile')
+        self._NTFS_tool.Extract_UsnJrnl()
+        files = ['$MFT', '$LogFile', '$UsnJrnl']
+        enc = hashlib.md5()
+        for file in files:
+            with open(file, 'rb') as f:
+                text = f.read()
+                enc.update(text)
