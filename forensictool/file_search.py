8f26572ccfe2c133349a6ec5ee59d5dd96cacd9a
20kee <20kee@naver.com>
2023-11-30 04:03:16 +0900

    All tools without Registry Completion

--- forensictool/file_search.py
+++ forensictool/file_search.py
@@ -1,9 +1,10 @@
 import os
+import shutil
 class FileExtensionSearchTool:
     def __init__(self, drive):
         self._drive = drive
 
-    def Search(self, route, extension):
+    def Search(self, route, extension, dest):
         try:
             dirs = os.listdir(route)
         except:
@@ -12,11 +13,11 @@ class FileExtensionSearchTool:
         for dir in dirs:
             sub_dir = os.path.join(route, dir)
             if os.path.isdir(sub_dir):
-                self.Search(sub_dir, extension)
+                self.Search(sub_dir, extension, dest)
             else:
                 file_name = dir.split('.')
                 if len(file_name) >= 2 and file_name[-1] == extension:
-                    print(sub_dir)
+                    shutil.copy2(sub_dir, dest + '\\' + dir)
 
 if __name__ == "__main__":
     search_tool = FileExtensionSearchTool('C')
--- forensictool/forensic_tool.py
+++ forensictool/forensic_tool.py
@@ -4,11 +4,13 @@ from forensictool.ntfs_forensic import *
 from forensictool.registry_forensic import *
 from forensictool.eventlog_forensic import *
 from forensictool.file_search import *
+from forensictool.browser_history import *
 from tkinter import filedialog
 import hashlib
 
 class ForensicTool:
-    def __init__(self, window_ver, drive):
+    def __init__(self, window_ver, drive, main_folder_name):
+        self._main_folder_name = main_folder_name
         self._drive = drive
         self._window_ver = window_ver
         self._recycle_bin_link = None
@@ -16,56 +18,77 @@ class ForensicTool:
         self._registry_tool = RegistryForensicTool()
         self._eventlog_tool = EventlogForensicTool()
         self._extension_search_tool = FileExtensionSearchTool(drive)
+        self._browser_tool = BrowserForensicTool()
 
     def SetLink(self):
-        if self._window_ver == "window 11":
-            self._recycle_bin_link = 'C:\\$Recycle.Bin'
+        self._recycle_bin_link = 'C:\\$Recycle.Bin'
     
     def RecycleBinForensic(self): # 휴지통 포렌식
-        result_file = open('휴지통 포렌식 결과.txt', 'w')
+        folder_name = "recycle_bin"
+        os.mkdir(self._main_folder_name + '\\' + folder_name)
         if True:
             dirs = os.listdir(self._recycle_bin_link) # 휴지통 안의 폴더들
             for dir in dirs:
                 try:
                     files = os.listdir(self._recycle_bin_link + '\\' + dir) # 내 진짜 휴지통 안의 삭제된 파일을
                     for file in files:
-                        if file[1] == 'I': # 삭제된 파일의 정보를 담고있는 파일
-                            f = open(self._recycle_bin_link + '\\' + dir + '\\' + file, 'rb')
-                            data = f.read()
-                            print(int.from_bytes(data[16:24], 'little') / 1000)
-                            result_file.write('파일명 : ' + file + '\n파일 헤더 : ' + str(int.from_bytes(data[:8], 'little')) + '\n원본 파일 크기 : ' + str(int.from_bytes(data[8:16], 'little')) + '\n삭제된 시간 : ' + str(datetime.fromtimestamp(int.from_bytes(data[16:24], 'little') / 1e9)) + '\n\n')
-                            # 삭제된 시간 수정 필요함
-                            f.close()
+                        f = open(self._recycle_bin_link + '\\' + dir + '\\' + file, 'rb')
+                        data = f.read()
+                        f2 = open(self._main_folder_name + "\\recycle_bin\\{}".format(file), 'wb')
+                        f2.write(data)
+                        f2.close()
+                        f.close()
 
                 except Exception as e:
                     print(e)
                     pass
         
         print('Recycle Bin Forensic Done.')
-        result_file.close()
 
     def SearchFileByExtension(self, extension): # 특정 확장자 파일들 검색
         root = filedialog.askdirectory(initialdir="/",\
 					title = "폴더를 선택 해 주세요")
-        self._extension_search_tool.Search(root, extension)
+        folder_name = "specific_extension_files"
+        os.mkdir(self._main_folder_name + '\\' + folder_name)
+        self._extension_search_tool.Search(root, extension, self._main_folder_name + '\\' + folder_name)
 
     def NTFSForensic(self): # NTFS 포렌식
-        self._NTFS_tool.Extract('/$MFT','$MFT')
-        self._NTFS_tool.Extract('/$LogFile','$LogFile')
-        self._NTFS_tool.Extract_UsnJrnl()
+        folder_name = 'ntfs'
+        os.mkdir(self._main_folder_name + '\\' + folder_name)
+        self._NTFS_tool.Extract('/$MFT',self._main_folder_name+'/ntfs/$MFT')
+        self._NTFS_tool.Extract('/$LogFile',self._main_folder_name+'/ntfs/$LogFile')
+        self._NTFS_tool.Extract_UsnJrnl(self._main_folder_name)
         files = ['$MFT', '$LogFile', '$UsnJrnl']
         enc = hashlib.md5()
         for file in files:
-            with open(file, 'rb') as f:
+            with open(self._main_folder_name+'/ntfs/'+file, 'rb') as f:
                 text = f.read()
                 enc.update(text)
+                print(enc.hexdigest())
 
     def EventlogForensic(self): # 이벤트로그 포렌식
-        pass
+        folder_name = 'event_log'
+        self._eventlog_tool.CoptEventLogs(self._main_folder_name + '\\' + folder_name)
 
-    def RegistryForensic(self): # 레지스트리 포렌식f
-        if user_keyword.lower() == 'all':
+    def RegistryForensic(self, registry_opt): # 레지스트리 포렌식f
+        if registry_opt == 'all':
             self._registry_tool.registry_all(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE")
         else:
-            self._registry_tool.registry_keyword(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", user_keyword)
+            self._registry_tool.registry_keyword(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", registry_opt)
+    
+    def ChromeForensic(self):
+        folder_name = 'chrome_history'
+        os.mkdir(self._main_folder_name +'\\' + folder_name)
+        self._browser_tool.copy_chrome_data(self._main_folder_name +'\\' + folder_name)
+    
+    def EdgeForensic(self):
+        folder_name = 'edge_history'
+        os.mkdir(self._main_folder_name +'\\' + folder_name)
+        self._browser_tool.copy_edge_data(self._main_folder_name +'\\' + folder_name)
+    
+
+    def FirefoxForensic(self):
+        pass
+
+
 
