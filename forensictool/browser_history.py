b33569e4e0491d6bf61d2f7db49f92abaf4c9fb0
76embiid21 <76embiid21@naver.com>
2023-11-29 23:23:09 +0900

    클래스 화했어요.. 화내지마요 창민이 오빠

--- forensictool/browser_history.py
+++ forensictool/browser_history.py
@@ -1,79 +1,56 @@
-import sqlite3
+#창민아 폴더명 클래스 호출할때 넣고 copy_browser_data(self) 이함수만 호출하면 작동됨
+
 import os
-from collections import Counter
+import shutil
+import hashlib
 
-def connect_to_chrome_history():
-    chrome_history_path = os.path.expanduser('~') + '/AppData/Local/Google/Chrome/User Data/Default/History'
-    try:
-        return sqlite3.connect(chrome_history_path)
-    except sqlite3.OperationalError as e:
-        print(f"데이터베이스 연결에 실패했습니다: {e}")
-        return None
-    
-def fetch_cookies():
-    cookies_path = os.path.expanduser('~') + '/AppData/Local/Google/Chrome/User Data/Default/Cookies'
-    try:
-        conn = sqlite3.connect(cookies_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT host_key, name, value FROM cookies;")
-        cookies = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        return cookies
-    except sqlite3.OperationalError as e:
-        print(f"쿠키 데이터베이스 연결에 실패했습니다: {e}")
-        return []    
+class BrowserHistory:
+    def __init__(self, destination_folder):
+        self.destination_folder = destination_folder
+        self.create_directory_if_not_exists(destination_folder)
+        self.base_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Local')
+        self.hash_log_file = os.path.join(destination_folder, 'hash.txt')
 
-def fetch_titles(conn):
-    cursor = conn.cursor()
-    cursor.execute("SELECT title FROM urls;")
-    titles = [title for (title,) in cursor.fetchall()]
-    cursor.close()
-    return titles
+    def create_directory_if_not_exists(self, directory):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-def fetch_custom_query(conn, query_str):
-    cursor = conn.cursor()
-    cursor.execute(query_str)
-    records = cursor.fetchall()
-    cursor.close()
-    return records
+    def calculate_file_hash(self, file_path):
+        hasher = hashlib.sha256()
+        with open(file_path, 'rb') as f:
+            buf = f.read()
+            hasher.update(buf)
+        return hasher.hexdigest()
 
-def extract_last_words(titles):
-    return [title.split()[-1] for title in titles if title.split()]
+    def copy_and_log_file_hash(self, src, dest):
+        shutil.copy2(src, dest)
+        file_hash = self.calculate_file_hash(dest)
+        with open(self.hash_log_file, 'a') as log:
+            log.write(f"{dest}: {file_hash}\n")
 
-def count_words(words):
-    return Counter(words)
+    def get_firefox_profiles(self):
+        firefox_base_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Roaming', 'Mozilla', 'Firefox', 'Profiles')
+        return [os.path.join(firefox_base_path, d) for d in os.listdir(firefox_base_path) if os.path.isdir(os.path.join(firefox_base_path, d))]
 
-if __name__ == "__main__":
-    browser = input("브라우저를 입력하세요 ex) chrome, edge, firefox: ")
+    def get_file_path(self, browser_name, profile_path, file_name):
+        if browser_name == 'firefox':
+            return os.path.join(profile_path, file_name)
+        else:
+            suffix = 'Network' if file_name == 'Cookies' else ''
+            return os.path.join(profile_path, browser_name.capitalize(), 'User Data', 'Default', suffix, file_name)
 
-    choice = input("인터넷 사용기록과 쿠키를 선택 하세요 ex) cookie, history: ")
-    if choice == "cookie":
-        cookies = fetch_cookies()
-        for host_key, name, value in cookies:
-            print(f"Host: {host_key}, Cookie Name: {name}, Value: {value}")
+    def copy_browser_data(self):
+        for browser_name in ['chrome', 'edge']:
+            for file_name in ['History', 'Cookies']:
+                file_path = self.get_file_path(browser_name, self.base_path, file_name)
+                if os.path.exists(file_path):
+                    destination = os.path.join(self.destination_folder, f'{browser_name}_{file_name}')
+                    self.copy_and_log_file_hash(file_path, destination)
 
-    elif choice == "history":
-        title_list = input("타이틀리스트들을 보여드릴까요? ex)Y,N: ")
-        conn = connect_to_chrome_history()
-        if conn is not None:
-            if title_list == "Y":
-                titles = fetch_titles(conn)
-                last_words = extract_last_words(titles)
-                word_counts = count_words(last_words)
-                for word, count in word_counts.items():
-                    print(f"{word}: {count}회")
-            else:
-                text = input("타이틀의 특정 문자열을 검색하세요: ")
-                date = input("검색할 날짜를 입력하세요: ")
-                conditions = []
-                if text:
-                    conditions.append(f"title LIKE '%{text}%'")
-                if date:
-                    conditions.append(f"visit_time BETWEEN '{date} 00:00:00' AND '{date} 23:59:59'")
-                query_str = f"SELECT url, datetime((last_visit_time/1000000)-11644473600, 'unixepoch', 'localtime') as visit_time, title FROM urls WHERE {' AND '.join(conditions)} ORDER BY last_visit_time;" if conditions else ""
-                records = fetch_custom_query(conn, query_str)
-                for url, visit_time, title in records:
-                    print(f"URL: {url}, 방문 시간: {visit_time}, 타이틀: {title}")
-
-            conn.close()
+        for profile_path in self.get_firefox_profiles():
+            for file_name in ['places.sqlite', 'cookies.sqlite']:
+                file_path = self.get_file_path('firefox', profile_path, file_name)
+                if os.path.exists(file_path):
+                    profile_name = os.path.basename(profile_path)
+                    destination = os.path.join(self.destination_folder, f'firefox_{profile_name}_{file_name}')
+                    self.copy_and_log_file_hash(file_path, destination)
