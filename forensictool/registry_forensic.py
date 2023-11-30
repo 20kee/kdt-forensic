1f6f0918318852baa3a63701d26fd40fc8b5ffd1
daniayo <jdy5619@ewhain.net>
2023-11-30 18:38:06 +0900

    .

--- forensictool/registry_forensic.py
+++ forensictool/registry_forensic.py
@@ -31,14 +31,26 @@ class RegistryForensicTool:
 
 
     def registry_all(self, hive, root_key):
-        subkeys_list = self.get_subkeys(hive, root_key)
+        starting_point = winreg.HKEY_LOCAL_MACHINE
+        subkeys = [
+            r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+            r"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+        ]
 
-        for subkey_name in subkeys_list:
-            full_subkey = f"{root_key}\\{subkey_name}"
-            value, value_type = self.read_registry_value(hive, full_subkey, "DisplayName")
+        for subkey in subkeys:
+            subkey_path = self.subkey
+            subkeys_list = self.get_subkeys(starting_point, subkey_path)
 
-            if value:
-                print(f"Found under {full_subkey}: {value} (Type: {value_type})")
+            if subkeys_list:
+                print(f"Subkeys under {subkey_path}: {subkeys_list}")
+
+                for subkey_name in subkeys_list:
+                    full_subkey = f"{subkey_path}\\{subkey_name}"
+                    value, value_type = self.read_registry_value(starting_point, full_subkey, "DisplayName")
+                    print(f"Value under {full_subkey}: {value} (Type: {value_type})")
+
+            else:
+                print(f"No subkeys found under {subkey_path}.")
 
     def registry_keyword(self, hive, root_key, keyword):
         # 특정 루트 키에서 시작하여 모든 하위 키 검색
@@ -54,12 +66,12 @@ class RegistryForensicTool:
                 print(f"Match found under {full_subkey}: {value} (Type: {value_type})")
 
 def main():
-    user_keyword = input("Enter keyword to search in registry (Enter 'all' to search the entire registry): ")
+    user_keyword = input("Enter keyword : ")
     registry_forensic_tool = RegistryForensicTool()
 
     # 검색 기능 : all 검색하면 다 나오고, keyword 따로 넣으면 검색됨
-    if user_keyword.lower() == 'all':
-        registry_forensic_tool.registry_all(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE")
+    if not user_keyword:
+        registry_forensic_tool.registry_all()
     else:
         registry_forensic_tool.registry_keyword(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", user_keyword)
 
