bde879b72cad4d7be2912f1feb12ed1f0903973e
76embiid21 <76embiid21@naver.com>
2023-11-30 11:58:50 +0900

    창민 오류 수정해놈 칭찬좀

--- forensictool/browser_history.py
+++ forensictool/browser_history.py
@@ -32,32 +32,15 @@ class BrowserForensicTool:
 
     def get_file_path(self, browser_name, profile_path, file_name=None):
         if browser_name == 'firefox':
-            return os.path.join(profile_path, file_name)
+            return os.path.join(profile_path, file_name) if file_name else None
         elif browser_name == 'chrome':
             suffix = 'Network' if file_name == 'Cookies' else ''
-            return os.path.join(profile_path, 'Google', browser_name.capitalize(), 'User Data', 'Default', suffix, file_name)
+            return os.path.join(profile_path, 'Google', 'Chrome', 'User Data', 'Default', suffix, file_name) if file_name else None
         elif browser_name == 'edge':
-            return os.path.join(profile_path, 'Microsoft', 'Windows', 'WebCache', 'WebCacheV01.dat')
+            suffix = 'Network' if file_name == 'Cookies' else ''
+            return os.path.join(profile_path, 'Microsoft', 'Edge', 'User Data', 'Default', suffix, file_name) if file_name else None
 
-    def copy_browser_data(self, dest):
-        print(1)
-        self.base_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Local')
-        self.hash_log_file = os.path.join(dest, 'hash.txt')
-        for browser_name in ['chrome', 'edge']:
-            for file_name in ['History', 'Cookies']:
-                file_path = self.get_file_path(browser_name, self.base_path, file_name)
-                if os.path.exists(file_path):
-                    self.copy_and_log_file_hash(file_path, dest)
-        try:
-            for profile_path in self.get_firefox_profiles():
-                for file_name in ['places.sqlite', 'cookies.sqlite']:
-                    file_path = self.get_file_path('firefox', profile_path, file_name)
-                    if os.path.exists(file_path):
-                        profile_name = os.path.basename(profile_path)
-                        destination = os.path.join(self.destination_folder, f'firefox_{profile_name}_{file_name}')
-                        self.copy_and_log_file_hash(file_path, destination)
-        except:
-            pass
+#chrome        
     def copy_chrome_data(self, dest):
         self.base_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Local')
         self.hash_log_file = os.path.join(dest, 'hash.txt')
@@ -70,17 +53,39 @@ class BrowserForensicTool:
                 except:
                     pass
     
+#edge    
     def copy_edge_data(self, dest):
         self.base_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Local')
         self.hash_log_file = os.path.join(dest, 'hash.txt')
         browser_name = 'edge'
         file_path = self.get_file_path(browser_name, self.base_path)
-        if os.path.exists(file_path):
-            print(file_path)
-            try:
-                self.copy_and_log_file_hash(file_path, dest)
-            except Exception as e:
-                print(e)
+        for file_name in ['History', 'Cookies']:
+            file_path = self.get_file_path(browser_name, self.base_path, file_name)
+            if os.path.exists(file_path):
+                try:
+                    self.copy_and_log_file_hash(file_path, dest)
+                except Exception as e:
+                    print(e)
 
+#파폭
     def copy_firefox_data(self, dest):
-        pass
+        self.create_directory_if_not_exists(dest)
+        self.base_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Roaming', 'Mozilla', 'Firefox', 'Profiles')
+        self.hash_log_file = os.path.join(dest, 'hash.txt')
+
+        for profile_path in self.get_firefox_profiles():
+            for file_name in ['places.sqlite', 'cookies.sqlite']:
+                file_path = self.get_file_path('firefox', profile_path, file_name)
+                if os.path.exists(file_path):
+                    profile_name = os.path.basename(profile_path)
+                    destination_file = f'firefox_{profile_name}_{file_name}'
+                    self.copy_and_log_file_hash(file_path, os.path.join(dest, destination_file))
+
+# 클래스 인스턴스 생성
+forensic_tool = BrowserForensicTool()
+
+# 목적지 폴더 지정
+destination_folder = os.path.join(os.path.expanduser('~'), 'Desktop', 'test','edge')
+
+# 메서드 호출
+forensic_tool.copy_chrome_data(destination_folder)
