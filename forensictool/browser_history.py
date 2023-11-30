7e059620ba1ea202a765b4c72d2610d8002a2ce3
76embiid21 <76embiid21@naver.com>
2023-11-30 11:00:26 +0900

    test

--- forensictool/browser_history.py
+++ forensictool/browser_history.py
@@ -4,12 +4,10 @@ import os
 import shutil
 import hashlib
 
-class BrowserHistory:
-    def __init__(self, destination_folder):
-        self.destination_folder = destination_folder
-        self.create_directory_if_not_exists(destination_folder)
-        self.base_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Local')
-        self.hash_log_file = os.path.join(destination_folder, 'hash.txt')
+class BrowserForensicTool:
+    def __init__(self):
+        self.base_path = None
+        self.hash_log_file = None
 
     def create_directory_if_not_exists(self, directory):
         if not os.path.exists(directory):
@@ -24,33 +22,65 @@ class BrowserHistory:
 
     def copy_and_log_file_hash(self, src, dest):
         shutil.copy2(src, dest)
-        file_hash = self.calculate_file_hash(dest)
-        with open(self.hash_log_file, 'a') as log:
-            log.write(f"{dest}: {file_hash}\n")
+        # file_hash = self.calculate_file_hash(dest)
+        # with open(self.hash_log_file, 'w') as log:
+        #     log.write(f"{dest}: {file_hash}\n")
 
     def get_firefox_profiles(self):
         firefox_base_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Roaming', 'Mozilla', 'Firefox', 'Profiles')
         return [os.path.join(firefox_base_path, d) for d in os.listdir(firefox_base_path) if os.path.isdir(os.path.join(firefox_base_path, d))]
 
-    def get_file_path(self, browser_name, profile_path, file_name):
+    def get_file_path(self, browser_name, profile_path, file_name=None):
         if browser_name == 'firefox':
             return os.path.join(profile_path, file_name)
-        else:
+        elif browser_name == 'chrome':
             suffix = 'Network' if file_name == 'Cookies' else ''
-            return os.path.join(profile_path, browser_name.capitalize(), 'User Data', 'Default', suffix, file_name)
+            return os.path.join(profile_path, 'Google', browser_name.capitalize(), 'User Data', 'Default', suffix, file_name)
+        elif browser_name == 'edge':
+            return os.path.join(profile_path, 'Microsoft', 'Windows', 'WebCache', 'WebCacheV01.dat')
 
-    def copy_browser_data(self):
+    def copy_browser_data(self, dest):
+        print(1)
+        self.base_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Local')
+        self.hash_log_file = os.path.join(dest, 'hash.txt')
         for browser_name in ['chrome', 'edge']:
             for file_name in ['History', 'Cookies']:
                 file_path = self.get_file_path(browser_name, self.base_path, file_name)
                 if os.path.exists(file_path):
-                    destination = os.path.join(self.destination_folder, f'{browser_name}_{file_name}')
-                    self.copy_and_log_file_hash(file_path, destination)
+                    self.copy_and_log_file_hash(file_path, dest)
+        try:
+            for profile_path in self.get_firefox_profiles():
+                for file_name in ['places.sqlite', 'cookies.sqlite']:
+                    file_path = self.get_file_path('firefox', profile_path, file_name)
+                    if os.path.exists(file_path):
+                        profile_name = os.path.basename(profile_path)
+                        destination = os.path.join(self.destination_folder, f'firefox_{profile_name}_{file_name}')
+                        self.copy_and_log_file_hash(file_path, destination)
+        except:
+            pass
+    def copy_chrome_data(self, dest):
+        self.base_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Local')
+        self.hash_log_file = os.path.join(dest, 'hash.txt')
+        browser_name = 'chrome'
+        for file_name in ['History', 'Cookies']:
+            file_path = self.get_file_path(browser_name, self.base_path, file_name)
+            if os.path.exists(file_path):
+                try:
+                    self.copy_and_log_file_hash(file_path, dest)
+                except:
+                    pass
+    
+    def copy_edge_data(self, dest):
+        self.base_path = os.path.join(os.path.expanduser('~'), 'AppData', 'Local')
+        self.hash_log_file = os.path.join(dest, 'hash.txt')
+        browser_name = 'edge'
+        file_path = self.get_file_path(browser_name, self.base_path)
+        if os.path.exists(file_path):
+            print(file_path)
+            try:
+                self.copy_and_log_file_hash(file_path, dest)
+            except Exception as e:
+                print(e)
 
-        for profile_path in self.get_firefox_profiles():
-            for file_name in ['places.sqlite', 'cookies.sqlite']:
-                file_path = self.get_file_path('firefox', profile_path, file_name)
-                if os.path.exists(file_path):
-                    profile_name = os.path.basename(profile_path)
-                    destination = os.path.join(self.destination_folder, f'firefox_{profile_name}_{file_name}')
-                    self.copy_and_log_file_hash(file_path, destination)
+    def copy_firefox_data(self, dest):
+        pass
